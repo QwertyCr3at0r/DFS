4656c7210ebfd19dea6ed6c9d419c18b91f7df47
QwertyCr3at0r <telegramemailwnwrrl@gmail.com>
2026-08-16 01:54:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import discord
+import os
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Бот запущен: {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Понг! {round(bot.latency * 1000)}ms")
+
+@bot.command()
+async def привет(ctx):
+    await ctx.send(f"Привет, {ctx.author.mention}!")
+
+bot.run(os.environ["TOKEN"])
